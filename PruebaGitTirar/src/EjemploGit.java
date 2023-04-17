e770421c2bb51d39cb7b409e201747fa7b5cf122
Eduardo <edu@pepe.com>
2023-04-18 01:32:28 +0200

    primer commit

---
+++ PruebaGitTirar/src/EjemploGit.java
@@ -0,0 +1,16 @@
+
+public class EjemploGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola");
+		
+		String nombre="Edu";
+		
+		System.out.println("Mi nombre es " + nombre);
+		
+		System.out.println("Hi");
+	}
+
+}
